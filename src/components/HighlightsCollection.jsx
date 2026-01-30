29cc7bbc5a138ac880e7277675d20aa56bec5e7f
ifelp <iranildofpersonal18@gmail.com>
2026-01-29 23:59:41 -0300

    feat(scroll space): making a horizontal scroll component to group the highlights tips.

---
+++ src/components/HighlightsCollection.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Highlights from "./Highlights";
+import { IconTitle } from "./IconTitle";
+
+/**
+ * 
+ ** @param {Object} props
+ * @param {Array<{id: number, image: any, description: string, icon: import("react").ElementType, title: string, size?: string}>} props.highlist - Array contendo o objeto com os elementos de um destaque.
+ * @returns 
+ */
+
+export default function HighlightsCollection({highlists}){
+    const highlistSize = highlists.length
+    return (
+        <div className=
+        {
+        `flex flex-row overflow-y-auto w-max-full 
+        ${highlistSize === 1 ? 'justify-center' : 'justify-start'} 
+        [scrollbar-width: thin] [&::-webkit-scrollbar]:w-2`
+        }>
+            {highlists.map((highlight)=> (
+                    <Highlights
+                    key={highlight.id}
+                    image={highlight.image}
+                    description={highlight.description}>
+                        <IconTitle 
+                        icon={highlight.icon} 
+                        title={highlight.title} 
+                        className='flex-row-reverse justify-between' 
+                        size="wrap"
+                        iconSize="18">
+                        </IconTitle>
+                    </Highlights>
+            ))}
+        </div>
+    )
+}
