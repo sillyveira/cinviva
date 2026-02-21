f0042578c8e4405189f5a6953ef964c4cc6f3d77
millena <millenaellensilva@gmail.com>
2026-02-21 02:15:48 -0300

    refactor: improve main page layout and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import Mercado from './pages/Mercado'
-import MainDesktop from './pages/MainDesktop'
 import TestPage from './pages/TestPage'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path='/desktop' element={<MainDesktop/>}/>
           <Route path="/test" element={<TestPage />} />
           <Route path="/mercado" element={<Mercado />} />
           <Route path="/mercados/:mercadoId" element={<Mercado />} />
