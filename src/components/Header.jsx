f0f61cca44c44d7f987b280e6e64cea15abd5420
condevictor <vddc@cin.ufpe.br>
2026-02-28 23:33:37 -0300

    fix overflow bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,17 @@ export default function Header() {
       className="hidden md:block w-full"
       style={{ backgroundColor: "#134abf" }}>
 
-      <div className="max-w-6xl mx-auto px-8 py-4 flex items-center justify-center">
+      <div className="max-w-6xl mx-auto px-8 py-3 flex items-center justify-start gap-4">
+        <img
+          src="/logo-pcr.svg"
+          alt="Logo PCR"
+          className="h-20"
+        />
         <img
           src="/logo-conecta.png"
-          alt="Logo Conviva"
+          alt="Logo Conecta"
           className="h-15"
         />
-
       </div>
     </header>
   );
